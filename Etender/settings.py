18aca38176eeb194eaa0c19356c0c2f9a7f16cd1
Jasurbek2003 <130648762+Jasurbek2003@users.noreply.github.com>
2024-10-11 18:36:37 +0500

    allowed hosts

--- Etender/settings.py
+++ Etender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^exf3ky+hety7#&s_j2vx*gxydu8dcgq(+c@!wuigzw!#i%b1h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
